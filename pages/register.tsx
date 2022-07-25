0dafdda68e4cb54f645b7e5df72ee2331e290069
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-07-25 20:14:40 +0530

    added post model 🛠

---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+const Register = ( ) => {
+    return(
+        <div className="flex min-h-screen flex-col items-center p-4 bg-black">
+            <h1 className="text-white font-bold text-4xl">Register</h1>
+            <div></div>
+        </div>
+    )
+}
+
+
+export default Register
