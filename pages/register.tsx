bde5603bc7938868c244ef3bc96e3e5d5d486fb6
Rahul Bhardwaj <rahul.03111999@gmail.com>
2022-07-26 08:39:09 +0530

    added register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,16 @@
 const Register = ( ) => {
     return(
-        <div className="flex min-h-screen flex-col items-center p-4 bg-black">
-            <h1 className="text-white font-bold text-4xl">Register</h1>
-            <div></div>
+        <div className="flex min-h-screen flex-col items-center justify-center p-4 bg-black">
+            <h1 className="font-bold text-4xl text-emerald-500">Register</h1>
+            <div>
+                <div className="p-4 flex flex-col">
+                    <input type="text" className="p-4 m-2 rounded-md" placeholder="Enter Name" />
+                    <input type="text" className="p-4 m-2 rounded-md" placeholder="Enter Username" />
+                    <input type="text" className="p-4 m-2 rounded-md" placeholder="Enter Email" />
+                    <input type="text" className="p-4 m-2 rounded-md" placeholder="Enter Password"/>
+                    <button className="rounded-md p-4 m-2 bg-emerald-500">Register</button>
+                </div>
+            </div>
         </div>
     )
 }
